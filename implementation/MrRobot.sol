c659167ebe5e2a3e0bebe987cc0bc7b1d274479f
Shivam <41016630+shivammusic@users.noreply.github.com>
2023-04-10 01:25:48 -0400

    Update MrRobot.sol

--- implementation/MrRobot.sol
+++ implementation/MrRobot.sol
@@ -5,3 +5,29 @@ pragma solidity ^0.8.13;
 contract MrRobot {
     mapping(address => uint) public balances;
  
+ 
+/*
+// Remove comment/Activate this code block to fix the vulnerability :) 
+bool internal locked;
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+     function withdraw() public noReentrant{
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+        balances[msg.sender] = 0;
+    }
+
+    function getBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
+
+// look at the comment on line 39 to fix the vulnerability :)
+
+*/
